4e1760185fd0d22aba5da6a658a8be2db4fb79b6
PaulCalnan <pcalnan71@gmail.com>
2017-11-22 05:29:42 +1100

    Added validations and topic form element for topics in blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -88,6 +88,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog) .permit(:title, :body) #can be overidden by having (permit_all_parameters = true) inserted within application.rb
+      params.require(:blog) .permit(:title, :body, :topic_id) #can be overidden by having (permit_all_parameters = true) inserted within application.rb
     end
 end
