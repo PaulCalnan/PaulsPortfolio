f087d9f579c5cd641cf5290bc2f105ce6646b9ca
PaulCalnan <pcalnan71@gmail.com>
2017-11-30 04:43:00 +1100

    Revert "Trying new config for Heroku"
    This reverts commit 97c0486823f0963013279c1ea0ff223d4b1cc379.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,13 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require bootstrap-sprockets
 //= require html.sortable
 //= require cocoon
 //= require gritter
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
