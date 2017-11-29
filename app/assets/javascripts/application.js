069f820078de8c6259bebf75ff80b789c43c49a4
PaulCalnan <pcalnan71@gmail.com>
2017-11-29 21:59:15 +1100

    reorder .js file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
@@ -20,3 +19,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap
