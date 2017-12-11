48a79a05619132a01322bced0f8831bb360548d4
PaulCalnan <pcalnan71@gmail.com>
2017-12-11 18:15:10 +1100

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = ['https://pauls-portfolio.herokuapp.com/', 'http://pauls-portfolio.herokuapp.com/']
+  config.action_cable.url = "wss://pauls-portfolio.herokuapp.com/cable"
 end
